3edcb2fa39ebab80de7bbff95a5e70e01f50463b
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-02-28 12:25:58 -0500

    Add first spark tries

---
+++ src/main/kotlin/org/janelia/saalfeldlab/spark/BlockwiseEdgeFeatures.kt
@@ -0,0 +1,138 @@
+package org.janelia.saalfeldlab.spark
+
+import gnu.trove.map.hash.TLongObjectHashMap
+import gnu.trove.set.TLongSet
+import gnu.trove.set.hash.TLongHashSet
+import net.imglib2.FinalInterval
+import net.imglib2.Interval
+import net.imglib2.img.array.ArrayImgs
+import net.imglib2.type.numeric.integer.UnsignedLongType
+import net.imglib2.type.numeric.real.FloatType
+import net.imglib2.util.Intervals
+import net.imglib2.view.Views
+import org.apache.spark.SparkConf
+import org.apache.spark.api.java.JavaSparkContext
+import org.janelia.saalfeldlab.edge.feature.DoubleStatisticsFeature
+import org.janelia.saalfeldlab.edge.feature.Histogram
+import org.janelia.saalfeldlab.labels.Label
+import org.janelia.saalfeldlab.n5.ByteArrayDataBlock
+import org.janelia.saalfeldlab.n5.DataType
+import org.janelia.saalfeldlab.n5.GzipCompression
+import org.janelia.saalfeldlab.n5.N5FSReader
+import org.janelia.saalfeldlab.n5.N5FSWriter
+import org.janelia.saalfeldlab.n5.N5Reader
+import org.janelia.saalfeldlab.n5.N5Writer
+import org.janelia.saalfeldlab.n5.imglib2.N5Utils
+import org.janelia.saalfeldlab.util.computeIfAbsent
+import org.slf4j.LoggerFactory
+import scala.Tuple2
+import scala.Tuple3
+import java.lang.invoke.MethodHandles
+import java.nio.ByteBuffer
+import kotlin.random.Random
+
+private val LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass())
+
+class N5IO(
+        val featureBlockContainer: N5Writer,
+        val weightsContainer: N5Reader = featureBlockContainer,
+        val labelsContainer: N5Reader = featureBlockContainer,
+        val edgesContainer: N5Writer = featureBlockContainer,
+        val weightsDataset: String,
+        val labelsDataset: String,
+        val featureBlockDataset: String,
+        val edgesDataset: String)
+
+class FeatureAndName(val feature: () -> DoubleStatisticsFeature<*>, val name: String)
+
+fun updateFeatureBlocks(
+        sc: JavaSparkContext,
+        n5io: () -> N5IO,
+        blocks: List<Tuple2<out Interval, LongArray>>,
+        vararg features: () -> DoubleStatisticsFeature<*>) {
+
+    val edges = TLongObjectHashMap<TLongSet>()
+    val perBlockEdges = sc
+            .parallelize(blocks.map{ Tuple3(Intervals.minAsLongArray(it._1()), Intervals.maxAsLongArray(it._1()), it._2()) })
+            .map {
+                val block = FinalInterval(it._1(), it._2())
+                val weights = Views.extendValue(N5Utils.open<FloatType>(n5io().weightsContainer, n5io().weightsDataset), FloatType(Float.NaN))
+                val labels = Views.extendValue(N5Utils.open<UnsignedLongType>(n5io().labelsContainer, n5io().labelsDataset), UnsignedLongType(Label.INVALID))
+                val writer = n5io().featureBlockContainer
+                var dataset = n5io().featureBlockDataset
+                val edgeFeatures = DoubleStatisticsFeature.addAll(weights, labels, block, features=*features)
+                val edgeMap = TLongObjectHashMap<LongArray>()
+                edgeFeatures.forEachEntry { a, b -> edgeMap.put(a, b.keySet().toArray()); true }
+                LOG.info("Edge features: {}", edgeFeatures)
+                // TLongObjectHashMap$KeyView does not have closing curly brace in string representation!!
+                LOG.info("Edge map: {}", edgeMap)
+
+                var requiredSize = 0
+                edgeFeatures.forEachEntry { e1, set ->  set.forEachEntry { e2, features -> requiredSize += 2 * java.lang.Long.BYTES + features.map(DoubleStatisticsFeature<*>::numBytes).sum(); true }; true }
+                val blockData = ByteArray(requiredSize)
+                val buffer = ByteBuffer.wrap(blockData)
+                edgeFeatures.forEachEntry { e1, set ->  set.forEachEntry { e2, features -> buffer.putLong(e1); buffer.putLong(e2); features.forEach { it.serializeInto(buffer) }; true }; true }
+                buffer.rewind()
+                val dataBlock = ByteArrayDataBlock(Intervals.dimensionsAsIntArray(block), it._3(), blockData)
+                n5io().featureBlockContainer.writeBlock(n5io().featureBlockDataset, n5io().featureBlockContainer.getDatasetAttributes(n5io().featureBlockDataset), dataBlock)
+
+                edgeMap
+            }
+            .collect()
+    perBlockEdges.forEach { it.forEachEntry { a, b -> edges.computeIfAbsent(a, {TLongHashSet()}).addAll(b); true } }
+    val edgesContainer = n5io().edgesContainer
+    val edgesDataset = n5io().edgesDataset
+    if (edgesContainer.datasetExists(edgesDataset)) {
+        val cursor = Views.flatIterable(N5Utils.open<UnsignedLongType>(edgesContainer, edgesDataset)).cursor()
+        while (cursor.hasNext())
+            edges.computeIfAbsent(cursor.next().integerLong, {TLongHashSet()}).add(cursor.next().integerLong)
+    }
+
+    val edgeCount = edges.valueCollection().map { it.size() }.reduce { i1, i2 -> i1 + i2}
+    val edgesRai = ArrayImgs.unsignedLongs(2L, edgeCount.toLong())
+    val edgesRaiCursor = Views.flatIterable(edgesRai).cursor()
+    edges.forEachEntry { e1, value -> value.forEach { edgesRaiCursor.next().setInteger(e1); edgesRaiCursor.next().setInteger(it); true }; true }
+
+    N5Utils.save(edgesRai, edgesContainer, edgesDataset, intArrayOf(2, edgeCount), GzipCompression())
+
+}
+
+fun main(args: Array<String>) {
+
+    val dims = longArrayOf(30, 40, 35)
+    val blockSize = dims.map { it.toInt() }.toIntArray()
+    val rng = Random(100L)
+    val randomLabels = ArrayImgs.unsignedLongs(*dims)
+    val randomEdges = ArrayImgs.floats(*dims)
+    randomLabels.forEach { it.setInteger(rng.nextLong(1, 11)) }
+    randomEdges.forEach { it.setReal(rng.nextFloat()) }
+
+    val path = "${System.getProperty("user.home")}/.local/tmp/edge-features.n5"
+    N5Utils.save(randomLabels, N5FSWriter(path), "labels", blockSize, GzipCompression())
+    N5Utils.save(randomEdges, N5FSWriter(path), "affinities", blockSize, GzipCompression())
+
+    N5FSWriter(path).createDataset("feature-blocks", dims, blockSize, DataType.INT8, GzipCompression())
+
+    val conf = SparkConf().setAppName(MethodHandles.lookup().lookupClass().simpleName)
+    val sc = JavaSparkContext(conf)
+    val n5io = {N5IO(
+            weightsContainer = N5FSReader(path),
+            weightsDataset = "affinities",
+            labelsDataset = "labels",
+            featureBlockContainer = N5FSWriter(path),
+            featureBlockDataset = "feature-blocks",
+            edgesDataset = "edges"
+    )}
+    sc.use {
+        updateFeatureBlocks(it, n5io, listOf(Tuple2(FinalInterval(*dims), longArrayOf(0, 0, 0))), {Histogram(5,max=1.0001)})
+    }
+
+    val dataBlock = N5FSReader(path).let{ it.readBlock("feature-blocks", it.getDatasetAttributes("feature-blocks"), longArrayOf(0, 0, 0)) as ByteArrayDataBlock}
+    val featureMap = TLongObjectHashMap<TLongObjectHashMap<List<DoubleStatisticsFeature<*>>>>()
+    val buffer = ByteBuffer.wrap(dataBlock.data)
+    while (buffer.hasRemaining()) {
+        featureMap.computeIfAbsent(buffer.long) {TLongObjectHashMap()}.put(buffer.long, Histogram(5, max=1.0001).let {it.deserializeFrom(buffer); listOf(it)})
+    }
+    LOG.info("Edge features: {}", featureMap)
+
+}
